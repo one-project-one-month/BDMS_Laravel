9d7e76b7d3856a54dcf14324ec7ebd33d50dc008
Aung Min Ko <aungminko951357.com>
2026-02-28 16:12:17 +0630

    feat:user_controller_add

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,6 @@ Route::prefix('v1/')->group(function () {
     Route::middleware(['auth:sanctum', 'Role.check:admin,staff'])->group(function () {
         Route::patch('users/{id}/deactivate', [UserController::class, 'deactivate']);
         Route::patch('users/{id}/activate', [UserController::class, 'activate']);
-        Route::apiResource('/users', UserController::class);
+        Route::apiResource('users', UserController::class);
     });
 });
