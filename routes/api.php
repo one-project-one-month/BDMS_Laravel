32f3bf15407fb26f8a6922b38e35eddb6c9a79d0
Khant Loon Thu <142862466+KhantLoonThu@users.noreply.github.com>
2026-03-02 00:32:26 +0700

    feat: implements authentication api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,19 @@
 use App\Http\Controllers\Api\Admin\AnnouncementController;
 use App\Http\Controllers\Api\Admin\DonorController;
 use App\Http\Controllers\Api\Admin\UserController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1/auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('me', [AuthController::class, 'me']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('logout-all', [AuthController::class, 'logoutAll']);
+    });
+});
 
 Route::prefix('v1/')->group(function () {
     Route::middleware(['auth:sanctum', 'Role.check:admin,staff'])->group(function () {
