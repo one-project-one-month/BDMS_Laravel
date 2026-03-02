5ca34fbe6f58a5dcbf3472b011145d48f7866ba0
PhonePyaeKo <phonepyaeko01@gmail.com>
2026-03-02 12:44:25 +0630

    fix/AppointmentResourceAndRequest

--- app/Http/Controllers/Api/Admin/AppointmentsController.php
+++ app/Http/Controllers/Api/Admin/AppointmentsController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Helpers\ApiResponse;
+use App\Http\Requests\Admin\AppointmentRequest;
 use App\Http\Resources\Api\Admin\AppointmentResource;
 use App\Models\Appointment;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class AppointmentsController extends Controller
 {
@@ -19,26 +19,15 @@ class AppointmentsController extends Controller
             ->latest()
             ->paginate(config('pagination.perPage'));
 
-        return $this->successResponse('Appointments Index', AppointmentResource::collection($appointments));
+        return $this->successResponse('Appointment Lists', AppointmentResource::collection($appointments));
     }
 
     //appointments update
-    public function update(Request $request, Appointment $appointment)
+    public function update(AppointmentRequest $request, Appointment $appointment)
     {
-        $validator = Validator::make($request->all(), [
-            'appointment_date' => 'required|date',
-            'appointment_time' => 'required|date_format:H:i',
-            'status' => 'required|in:scheduled, cancelled, confirmed, completed',
-            'remarks' => 'nullable|string',
-        ]);
-
-        if($validator->fails()) {
-            return $this->errorResponse("Validation Error", 422);
-        }
-
         try {
 
-            $appointment->update($validator->validated());
+            $appointment->update($request->validated());
 
             return $this->successResponse("Appointment Updated Successfully!", new AppointmentResource($appointment));
 
